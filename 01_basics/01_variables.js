32348a793950022744386fe470e153bb2cbd4747
shahrokh0796 <shahrokh07.96@gmail.com>
2023-10-28 09:28:48 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553;
+let accouneEmail = "shahrokh07.it@gmail.com";
+var accountPassword = "12345";
+accountCity = "Delhi";
+let accountState;
+
+// accountId = 2;
+accouneEmail = "shah@bmail.com";
+accountPassword = "678910"
+accountCity = "agra";
+console.log(accountId);
+console.table([accountId, accouneEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
