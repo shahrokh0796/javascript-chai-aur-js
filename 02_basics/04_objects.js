e63ca8c4e7f3e43e01191d807cdf6bd04ba7be98
shahrokh0796 <shahrokh07.96@gmail.com>
2023-10-30 09:23:32 +0000

    obects part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,68 @@
+const tinderUser = new Object();
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gamil.com",
+    fullname: {
+        userfullname: {
+            firstName: "Said",
+            lastName: "Temori"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+const obj3 = {5: "c", 6: "d"};
+// const obj3 = {obj1, obj2}; // wrong 
+// console.log(obj3);
+
+// const obj4 = Object.assign({}, obj1, obj2);
+// const obj4 = Object.assign({}, obj1, obj2, obj3);
+// console.log(obj4);
+// const obj4 = {...obj1, ...obj2, ...obj3};
+// console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email: "S@gmail.com",
+    },
+    {
+        id: 1,
+        email: "S@gmail.com",
+    },
+    {
+        id: 1,
+        email: "S@gmail.com",
+    },
+    {
+        id: 1,
+        email: "S@gmail.com",
+    },
+];
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty("isLogged"));
+
+
+
+
+
+
+
+
+
+
