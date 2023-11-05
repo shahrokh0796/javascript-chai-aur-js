25ff02e9668653763480750146a5330ef25ea14e
shahrokh0796 <shahrokh07.96@gmail.com>
2023-11-05 04:43:27 +0000

    first project solution

---
+++ 07_projects/color-changer/script.js
@@ -0,0 +1,24 @@
+const btns = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        console.log(e);
+        console.log(e.target);
+
+        switch(e.target.id) {
+            case 'grey':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'white':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'blue':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'yellow':
+                body.style.backgroundColor = e.target.id;
+                break;
+        }
+    });
+});
